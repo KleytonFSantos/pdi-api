25889e1481a4f3282f1d51b9c345fd89762f8636
kleytonsantos <akatsukipb12@gmail.com>
2024-08-10 21:11:31 -0300

    feat: creating schemas for swagger documentation

--- src/Domain/DTO/TransactionDTO.php
+++ src/Domain/DTO/TransactionDTO.php
@@ -2,9 +2,16 @@
 
 namespace App\Domain\DTO;
 
+use OpenApi\Attributes\Property;
+use Symfony\Component\Serializer\Attribute\Groups;
+
 class TransactionDTO
 {
+    #[Groups(["create"])]
+    #[Property(type: 'float')]
     private float $value;
+    #[Groups(["create"])]
+    #[Property(type: 'number')]
     private int $payee;
 
     public function getValue(): float
